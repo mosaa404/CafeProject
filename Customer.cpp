33c3c928b159416ddd41e1f1a3b085f475868d45
erinigaid <57288259+erinigaid@users.noreply.github.com>
2019-12-14 22:23:19 +0200

    Add files via upload

--- Customer.cpp
+++ Customer.cpp
@@ -22,15 +22,17 @@ using namespace std;
 
 	void Customer::add_pill(string name, float total){
 		double discout= (total - ((0.1) * total));
+		double befTotal=total;
 		ofstream outfile("customer.dat", ios::app);
 		if (dcount(total) == 1) {
-			cout << "Your pill is " << total << " AFTER  DISCOUNT " << discout << endl;  //discount is 10%
-			outfile << "name : " << name << " total : " << discount << endl;
+			total=discout;
 		}
 		else {
-			cout << "Your Pill is  " << total << " AFTER  DISCOUNT " << (total) << endl;
-			outfile << "name : " << name << " total : " << total << endl;
+			total=befTotal;
+			
 		}
+			cout << "Your Pill is  " << befTotal << " AFTER  DISCOUNT " << (total) << endl;
+		outfile << "name : " << name << " total : " << total << endl;
 		outfile.close();
 	}
 
